c89228501d21ef5042bc69847c41735b32b67035
Markus Kollers <m.kollers@vertical.de>
2018-07-17 17:43:42 +0200

    Added header

--- src/app/shared/layout/layout.module.ts
+++ src/app/shared/layout/layout.module.ts
@@ -4,6 +4,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule } from '@angular/material/icon';
 
 import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 import { SocialFooterComponent } from './components/social-footer/social-footer.component';
 
 @NgModule({
@@ -15,10 +16,12 @@ import { SocialFooterComponent } from './components/social-footer/social-footer.
     MatIconModule
   ],
   declarations: [
+    HeaderComponent,
     FooterComponent,
     SocialFooterComponent
   ],
   exports: [
+    HeaderComponent,
     FooterComponent,
     SocialFooterComponent
   ]
