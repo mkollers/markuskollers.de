a99e504f099b3d070aadfce8f5ce3c80f6801771
Markus Kollers <m.kollers@vertical.de>
2019-07-06 19:56:30 +0200

    Init landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/landing-page/landing-page.module').then(m => m.LandingPageModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
