7cba02ff31a6395e19283cf2d8a7bca95a438292
Markus Kollers <m.kollers@vertical.de>
2019-07-07 01:30:33 +0200

    Style header

--- src/app/shared/layout/components/header/header.component.ts
+++ src/app/shared/layout/components/header/header.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  @HostBinding('class.small') isSmall = false;
 
   constructor() { }
 
-  ngOnInit() {
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    if (window.pageYOffset > 64) {
+      this.isSmall = true;
+    } else {
+      this.isSmall = false;
+    }
   }
 
 }
--- src/app/shared/layout/layout.module.ts
+++ src/app/shared/layout/layout.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+
+    // Material
+    MatToolbarModule
   ]
 })
 export class LayoutModule { }
