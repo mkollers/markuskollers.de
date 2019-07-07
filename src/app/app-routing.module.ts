c0b79f2eeacff54fe25723022d3ae250760a3690
Markus Kollers <m.kollers@vertical.de>
2019-07-07 21:03:46 +0200

    Add empty blog page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./pages/landing-page/landing-page.module').then(m => m.LandingPageModule) }
+  { path: '', loadChildren: () => import('./pages/landing-page/landing-page.module').then(m => m.LandingPageModule) },
+  { path: 'blog', loadChildren: () => import('./pages/blog-page/blog-page.module').then(m => m.BlogPageModule) }
 ];
 
 @NgModule({
