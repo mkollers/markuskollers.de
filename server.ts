7de55ff85957a5cc0734bce5e260119620dd782a
Markus Kollers <m.kollers@vertical.de>
2019-07-16 18:04:00 +0200

    Redirect from www to non www

--- server.ts
+++ server.ts
@@ -14,12 +14,11 @@
  * from your application's main.server.ts file, as seen below with the
  * import for `ngExpressEngine`.
  */
-
 import 'zone.js/dist/zone-node';
 
-import * as express from 'express';
-import {join} from 'path';
 import * as compression from 'compression';
+import * as express from 'express';
+import { join } from 'path';
 
 // Express server
 const app = express();
@@ -30,7 +29,7 @@ const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist/browser');
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap} = require('./dist/server/main');
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap } = require('./dist/server/main');
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine('html', ngExpressEngine({
@@ -43,6 +42,15 @@ app.engine('html', ngExpressEngine({
 app.set('view engine', 'html');
 app.set('views', DIST_FOLDER);
 
+// Redirect from www to non www
+app.get('/*', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  if (req.headers.host.match(/^www/) !== null) {
+    res.redirect('https://' + req.headers.host.replace(/^www\./, '') + req.url);
+  } else {
+    next();
+  }
+});
+
 // Example Express Rest API endpoints
 // app.get('/api/**', (req, res) => { });
 // Serve static files from /browser
@@ -51,7 +59,7 @@ app.get('*.*', express.static(DIST_FOLDER, {
 }));
 
 // All regular routes use the Universal engine
-app.get('*', (req, res) => {
+app.get('*', (req: express.Request, res: express.Response) => {
   res.render('index', { req });
 });
 
