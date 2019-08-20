eb257650ec18b1341304b986e51040d8cdecf2df
Markus Kollers <m.kollers@vertical.de>
2019-08-21 01:45:38 +0200

    Redirect to root on 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/landing-page/landing-page.module').then(m => m.LandingPageModule) },
   { path: 'blog', loadChildren: () => import('./pages/blog-page/blog-page.module').then(m => m.BlogPageModule) },
   { path: 'imprint', loadChildren: () => import('./pages/imprint-page/imprint-page.module').then(m => m.ImprintPageModule) },
-  { path: 'privacy', loadChildren: () => import('./pages/privacy-page/privacy-page.module').then(m => m.PrivacyPageModule) }
+  { path: 'privacy', loadChildren: () => import('./pages/privacy-page/privacy-page.module').then(m => m.PrivacyPageModule) },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
