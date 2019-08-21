547f78920abe675d40aa1bdbd9a64cfde49fb666
Markus Kollers <m.kollers@vertical.de>
2019-08-21 03:04:55 +0200

    Improve test coverage of app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,24 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { async, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { DomSanitizerMock } from 'src/mocks/dom-sanitizer.mock';
 import { MatIconRegistryMock } from 'src/mocks/mat-icon-registry.mock';
 
 import { AppComponent } from './app.component';
+import { RouterOutlet } from '@angular/router';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        NoopAnimationsModule
       ],
       declarations: [
         AppComponent
@@ -25,9 +31,26 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should return the path of the router outlet', () => {
+    // Arrange
+    const path = 'fancy-path';
+    const outlet: any = { activatedRouteData: { path } };
+
+    // Act
+    const state = component.getState(outlet);
+
+    // Assert
+    expect(state).toBe(path);
   });
 });
