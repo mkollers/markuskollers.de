a0dcf74245b565800557bde9a2c7d5126dcd8cb1
Markus Kollers <m.kollers@vertical.de>
2020-05-26 23:11:23 +0200

    Fix SSR

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,7 +1,6 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
@@ -10,8 +9,7 @@ import { UniversalInterceptor } from './shared/helper/interceptors/universal-int
 @NgModule({
   imports: [
     AppModule,
-    ServerModule,
-    ModuleMapLoaderModule
+    ServerModule
   ],
   bootstrap: [AppComponent],
   providers: [{
